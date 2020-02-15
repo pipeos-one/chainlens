526a3b36b38551e312600b97464634319c752e08
Loredana <loredana.cirstea@gmail.com>
2020-02-15 03:37:42 +0200

    Client - change from next.js to react-app-rewired & NativeBase

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Container, Content } from 'native-base';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Content>
+
+        </Content>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
