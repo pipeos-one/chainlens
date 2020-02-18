5b06677b726c40923287b749031549e85b64b1b5
Loredana <loredana.cirstea@gmail.com>
2020-02-18 05:09:59 +0200

    Client: add pfunction search for inputs/outputs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { buildWhereQueries, buildWhereFx } from './utils.js';
 import { exportToRemix, exportPclassToRemix } from './remix.js';
 
 const MIN_WIDTH = 800;
-const PAGE_LIMIT = 10;
+const PAGE_LIMIT = 30;
 
 function getPageSize(noOfPages, {width, height}) {
   console.log('--dimensions', noOfPages, {width, height});
@@ -55,17 +55,79 @@ function WorkTree(props) {
   )
 }
 
-function SearchList(props) {
-  const { whereFilters, filter } = props;
-  filter.where = whereFilters.pclassWhere;
+function SearchListPClasses(props) {
+  const { whereFilter, filter } = props;
 
-  const { data, error } = useSearchResults(filter);
-  const { data: count } = useSearchCount(filter);
+  filter.where = whereFilter;
+
+
+  const { data, error } = useSearchResults(filter, 'pclass');
+  const { data: count } = useSearchCount(filter, 'pclass');
+  const resultsCount = count ? count.count : 0;
 
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
 
+
+  return (
+    <View style={{ ...props.styles, flex: 1 }}>
+      <PclassList
+        data={data}
+        onSelect={props.onSelect}
+        onAddListPage={() => (resultsCount >= (filter.skip + filter.limit)) ? props.onAddListPage() : () => {}}
+        onPreviousPage={props.onPreviousPage}
+      />
+
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 5,
+        borderTopWidth: 1,
+        borderTopColor: '#cccccc',
+      }}>
+        <Button small rounded style={ styles.buttonStyle } onClick={props.onGoToSearch}>
+          <Icon name='search' />
+          <Text>{resultsCount}</Text>
+        </Button>
+        <Text>
+          @{(filter.skip || 0) + 1}
+        </Text>
+        <Button small rounded style={ styles.buttonStyle } onClick={props.onGoToWorkTree}>
+          <Text>{props.treedataLen || 0}</Text>
+          <Icon type="MaterialCommunityIcons" name='import' />
+        </Button>
+      </View>
+
+    </View>
+  )
+}
+
+function SearchListPfunctions(props) {
+  const { whereFilter, filter } = props;
+  filter.where = whereFilter;
+  filter.limit = 100;
+
+  const { data: pfuncData, error } = useSearchResults(filter, 'pfunction');
+  const { data: count } = useSearchCount(filter, 'pfunction');
   const resultsCount = count ? count.count : 0;
+
+  let indexes = {};
+  let data = [];
+  (pfuncData || []).forEach(pfunc => {
+    const pclass = pfunc.pclass;
+    console.log('pclass._id', pclass._id, indexes[pclass._id]);
+    if (!indexes[pclass._id] && indexes[pclass._id] !== 0) {
+      pclass.pfunctions = [pfunc];
+      indexes[pclass._id] = data.length;
+      data.push(pclass);
+    } else {
+      data[indexes[pclass._id]].pfunctions.push(pfunc);
+    }
+    });
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
 
   console.log('data', data)
   console.log('count', count)
@@ -193,6 +255,8 @@ class AppContent extends Component {
     const { pclassWhere, pfunctionWhere, pclassiWhere, filter } = this.state;
     const whereFilters = { pclassWhere, pfunctionWhere, pclassiWhere };
 
+    const showFunctions = (Object.keys(pfunctionWhere)[0] || '').includes('data.gapi');
+
     return (
       <ScrollView
         ref={this.scrollRef}
@@ -207,17 +271,32 @@ class AppContent extends Component {
 
         <SearchComponent styles={pageStyles} onQueryChange={this.onQueryChange} />
 
-        <SearchList
-          whereFilters={whereFilters}
-          filter={filter}
-          styles={pageStyles}
-          treedataLen={treedata.length}
-          onAddListPage={this.onAddListPage}
-          onPreviousPage={this.onPreviousPage}
-          onSelect={this.onSelect}
-          onGoToSearch={this.onGoToSearch}
-          onGoToWorkTree={this.onGoToWorkTree}
-        />
+        {showFunctions
+          ? <SearchListPfunctions
+            whereFilters={whereFilters}
+            whereFilter={pfunctionWhere}
+            filter={filter}
+            styles={pageStyles}
+            treedataLen={treedata.length}
+            onAddListPage={this.onAddListPage}
+            onPreviousPage={this.onPreviousPage}
+            onSelect={this.onSelect}
+            onGoToSearch={this.onGoToSearch}
+            onGoToWorkTree={this.onGoToWorkTree}
+          />
+          : <SearchListPClasses
+            whereFilters={whereFilters}
+            whereFilter={pclassWhere}
+            filter={filter}
+            styles={pageStyles}
+            treedataLen={treedata.length}
+            onAddListPage={this.onAddListPage}
+            onPreviousPage={this.onPreviousPage}
+            onSelect={this.onSelect}
+            onGoToSearch={this.onGoToSearch}
+            onGoToWorkTree={this.onGoToWorkTree}
+          />
+        }
 
         <WorkTree
           treedata={treedata}
--- src/fetchers.js
+++ src/fetchers.js
@@ -1,25 +1,34 @@
 import {PIPEOS_SERVER} from './config';
 import fetch from 'unfetch';
-import useSWR from 'swr';
+import useSWR, { useSWRPages } from "swr";
 
 const DEFAULT_OPTIONS = {
   revalidateOnFocus: false,
+  revalidateOnReconnect: false,
   dedupingInterval: 20000,
-  focusThrottleInterval: 50000,
+  focusThrottleInterval: 500000,
+  loadingTimeout: 5000,
+  errorRetryInterval: 10000,
 }
 
 export const pclassApi = () => `${PIPEOS_SERVER.host}${PIPEOS_SERVER.route.pclass}`;
 
-export const pclassWithPfuncApi = (filter = {}) => {
-  filter = {...filter, include: [{relation: 'pfunctions'}]};
+export const pfunctionApi = () => `${PIPEOS_SERVER.host}${PIPEOS_SERVER.route.pfunction}`;
+
+export const pclassWithPfuncApi = (filter = {}, type = 'pclass') => {
+  let relation = {relation: 'pfunctions'};
+  if (type === 'pfunction') {
+    relation.relation = 'pclass';
+  }
+  filter = {...filter, include: [relation]};
 
   console.log('filter', filter, JSON.stringify(filter));
-  return `${pclassApi()}?filter=${JSON.stringify(filter)}`;
+  return `${PIPEOS_SERVER.host}/${type}?filter=${JSON.stringify(filter)}`;
 }
 
-export const pclassCount = (filter = {}) => {
+export const pclassCount = (filter = {}, type) => {
   console.log('pclassCount', filter.where, JSON.stringify(filter.where));
-  return `${pclassApi()}/count?where=${JSON.stringify(filter.where)}`;
+  return `${PIPEOS_SERVER.host}/${type}/count?where=${JSON.stringify(filter.where)}`;
 }
 
 export async function fetcher(apiUrl) {
@@ -28,10 +37,10 @@ export async function fetcher(apiUrl) {
   return await res.json();
 }
 
-export function useSearchResults(filter) {
-  return useSWR(pclassWithPfuncApi(filter), fetcher, DEFAULT_OPTIONS)
+export function useSearchResults(filter, type = 'pclass') {
+  return useSWR(pclassWithPfuncApi(filter, type), fetcher, DEFAULT_OPTIONS)
 }
 
-export function useSearchCount(filter) {
-  return useSWR(pclassCount(filter), fetcher, DEFAULT_OPTIONS)
+export function useSearchCount(filter, type = 'pclass') {
+  return useSWR(pclassCount(filter, type), fetcher, DEFAULT_OPTIONS)
 }
