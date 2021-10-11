c087801863d6c314660b6b9319e1f61e97dcf1f8
Loredana <loredana.cirstea@gmail.com>
2021-10-11 12:38:39 +0300

    Client deployment fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const PIPEOS_SERVER = {
-  host: process.env.REACT_APP_PIPEOS_SERVER,
+  host: process.env.REACT_APP_PIPEOS_SERVER || 'https://chainlens.herokuapp.com',
   route: {
     pclass: '/pclass',
     pfunction: '/pfunction',
